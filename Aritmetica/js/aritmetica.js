bf80a89672d2aeb9baa792d4f6a88d0a2a20bfa9
ghost6829 <148910700+ghost6829@users.noreply.github.com>
2023-10-25 13:18:31 -0600

    Add files via upload

---
+++ Aritmetica/js/aritmetica.js
@@ -0,0 +1,31 @@
+function sumar() {
+    var x,y,suma,texto;
+    x = document.getElementById("num1").value;
+    y = document.getElementById("num2").value;
+
+    if(isNaN(x) || isNaN(y)){
+       texto = "es necesario escribir 2 numeros"
+    }
+
+    else{
+        suma = parseFloat(x) + parseFloat(y);
+        texto = "Resultado: " + suma;
+    }
+    document.getElementById("resultado").innerHTML = texto;
+}
+
+function restar() {
+    var x,y,restar,texto;
+    x = document.getElementById("num1").value;
+    y = document.getElementById("num2").value;
+
+    if(isNaN(x) || isNaN(y)){
+       texto = "es necesario escribir 2 numeros"
+    }
+
+    else{
+        restar = parseFloat(x) - parseFloat(y);
+        texto = "Resultado: " + restar;
+    }
+    document.getElementById("resultado").innerHTML = texto;
+}
---
+++ Bienvenido 2/Bienvenido.js
@@ -0,0 +1,8 @@
+
+function tituloNegro(){
+    document.getElementById("titulo").style ="color: black "; 
+    }
+    function tituloRojo(){
+    document.getElementById("titulo").style ="color: red"; 
+    }
+
